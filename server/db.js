24f679e0fddcf359ae25271701ba02138804d6a6
九日 <40729244+SumilerJR@users.noreply.github.com>
2022-09-18 16:13:37 +0800

    Create db.js

--- server/db.js
+++ server/db.js
@@ -8,10 +8,10 @@ function createConnection() {
     // 连接数据库
     return mysql.createConnection({
         host: 'localhost',         //服务器地址，连接本机可以使用localhost或者127.0.0.1
-        user: 'root',              //连接数据库的用户名
-        password: 'jiuri957',               //连接数据库的密码
-        database: 'checkin',          //数据库的名称
-        port: 13306                 //数据库端口号，默认3306，可以省略不写
+        user: '',              //连接数据库的用户名
+        password: '',               //连接数据库的密码
+        database: '',          //数据库的名称
+        port: 3306                 //数据库端口号，默认3306，可以省略不写
     });
 }
 
